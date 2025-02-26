472bc2b10e6db06ca791dc5eaa83d13057938cce
Luiz Gabriel Lisboa Santos <lzg.lisboasantos5569@gmail.com>
2025-02-25 21:23:04 -0300

    estilizando, adicionando cores e adicionando botões de concluir e deletar

--- src/components/ItemQuadro.jsx
+++ src/components/ItemQuadro.jsx
@@ -1,12 +1,20 @@
 import "./ItemQuadro.css"
 
-const itensQuadro = ["Lavar louça", "Estudar React", "Fazer compras", "Lavar roupa"]
+const itensQuadro = ["Lavar louça", "Estudar React", "Fazer compras", "Lavar roupa", "Estudar Inglês", "Fazer exercícios", "Limpar a casa", "Estudar Python", "Fazer almoço", "Estudar JavaScript"]
 
 const ItemQuadro = () => {
     return (
         <ul>
             {itensQuadro.map((item, index) => (
-                <li key={index}>{item}</li>
+                <>
+                    <li key={index}>{item}
+                        <div className="botoes">
+                            <button className="botao-concluir">Concluir</button>
+                            <button className="botao-deletar">Deletar</button> 
+                        </div>
+                    </li>
+                    
+                </>
             ))}
         </ul>
     )
